1fce99d215e3e953e3d5df1e52e62b084220ff17
Matt Swensen <swensen.matthew@gmail.com>
2019-10-26 22:47:31 -0600

    Alert the user when a new version has been cached

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: () => {
+    window.alert(
+      'A new version of Cornhole is available and has been downloaded in the background. Close and re-open to use the latest version.',
+    );
+  },
+});
